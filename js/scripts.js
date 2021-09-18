c8bfdceb6eccff10dc21cc5885b04e67c4720b2e
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-09-18 08:51:50 +0300

    order Constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
+/*function displayMore(){
+    var content = document.getElementsByClassName("content");
+    var button = document.getElementsByClassName("submitbtn");
+    if (content.style.display === "none") {
+        button.innerHTML = "order"
+        
+    } else {
+        button.innerHTML = "Add to order"
+    }
+    event.preventDefault();
+}*/
+
+
+
 $(document).ready(function(){
      
     $(".submitbtn").click(function(){
-        $(".content").toggle("slow",function(){$(".submitbtn").toggle()})
-        event.preventDefault();
+        $(".content1").slideToggle()
+        $(".content2").slideToggle()
+        $(".content3").slideToggle()
     })
+        event.preventDefault();
 
 
 
